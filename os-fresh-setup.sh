97edcc4f0637f054d88144cea6938d137136086c
Walter Hu <alterhu2020@gmail.com>
2021-03-12 13:13:57 +0800

    fix: docker issues

--- os-fresh-setup.sh
+++ os-fresh-setup.sh
@@ -121,27 +121,27 @@ mv "${CURRENT_FOLDER}/software/nginx/sites-enabled/example.com.conf" "${CURRENT_
 # run the nginx
 docker network create --driver overlay nginx-network
 docker build -f "${CURRENT_FOLDER}/software/nginx/Dockerfile" -t custom/nginx:latest "${CURRENT_FOLDER}/software/nginx/"
-#docker service create \
-#        --network nginx-network \
-#        --publish mode=host,published=80,target=80 \
-#        --mount src=/etc/nginx,dst=/etc/nginx \
-#        --mount src=/run/nginx.pid,dst=/run/nginx.pid \
-#        --mount src=/var/log/nginx,dst=/var/log/nginx \
-#        --mount src=/etc/letsencrypt,dst=/etc/letsencrypt \
-#        --mount src=/var/www/_letsencrypt,dst=/var/www/_letsencrypt \
-#        --mount src=/opt/workspace,dst=/opt/workspace \
-#        --replicas=1 \
-#        custom/nginx:latest
-#
-#certbot certonly --webroot -d "$WEBSIT_NAME" -d "www.$WEBSIT_NAME" --email alterhu2020@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
-#
-#sed -i -r 's/#?;#//g' /etc/nginx/sites-enabled/"$WEBSIT_NAME".conf
-#docker service create --replicas=1 custom/nginx:latest
-#
-# rm -rf /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
-#echo -e '#!/bin/bash\nnginx -t && systemctl reload nginx' | sudo tee /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
-#sudo chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
-#docker service create --replicas=1 custom/nginx:latest
+docker service create \
+        --network nginx-network \
+        --publish mode=host,published=80,target=80 \
+        --mount src=/etc/nginx,dst=/etc/nginx,type=bind \
+        --mount src=/run/nginx.pid,dst=/run/nginx.pid,type=bind \
+        --mount src=/var/log/nginx,dst=/var/log/nginx,type=bind \
+        --mount src=/etc/letsencrypt,dst=/etc/letsencrypt,type=bind \
+        --mount src=/var/www/_letsencrypt,dst=/var/www/_letsencrypt,type=bind \
+        --mount src=/opt/workspace,dst=/opt/workspace,type=bind \
+        --replicas=1 \
+        custom/nginx:latest
+
+certbot certonly --webroot -d "$WEBSIT_NAME" -d "www.$WEBSIT_NAME" --email alterhu2020@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+
+sed -i -r 's/#?;#//g' /etc/nginx/sites-enabled/"$WEBSIT_NAME".conf
+docker service create --replicas=1 custom/nginx:latest
+
+rm -rf /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
+echo -e '#!/bin/bash\nnginx -t && systemctl reload nginx' | sudo tee /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
+sudo chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
+docker service create --replicas=1 custom/nginx:latest
 #================================================
 # at last, clear the memory
 sh -c "echo 3 > /proc/sys/vm/drop_caches"
