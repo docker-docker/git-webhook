b1b5117138fb125306c85f0d2829e4606b2dd7e1
Walter Hu <alterhu2020@gmail.com>
2021-09-15 11:15:56 +0800

    fix: docker step

--- software/docker.sh
+++ software/docker.sh
@@ -14,12 +14,15 @@ sed -i -r 's/(ExecStart*)/#\1/g' /lib/systemd/system/docker.service
 sed -i -r '/^#ExecStart=.*/a ExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2857' /lib/systemd/system/docker.service
 systemctl daemon-reload
 systemctl restart docker
+systemctl enable docker
 echo "Docker installed, set mirror to aliyuncs, open the docker tcp connection for docker swarm!"
 usermod -aG docker root
 
 # 4.1 docker-compose
-curl -L --fail https://github.com/docker/compose/releases/download/1.28.5/run.sh -o /usr/local/bin/docker-compose
+pip install docker-compose
 chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 echo "Docker compose installed"
 # 4.2 docker swarm
 #docker swarm init
